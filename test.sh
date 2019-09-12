b52e36cb84e23d73639220c62bb302d4f1d49767
pablo bullian <pablo.bullian@gmail.com>
2019-09-12 18:36:45 -0300

    commit inicial

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+var="UNSAM administracion GNU/linux"
+aux=$var
+echo Hash de \"UNSAM administracion GNU/linux\":
+echo $var | sha1sum
+sha=1
+echo Calculando...
+while [ `echo $sha | cut -c1` != "0" ]
+do
+    
+    aux=$aux$RANDOM
+	echo $aux
+    sha=`echo $aux | sha1sum`
+done
+echo $sha
